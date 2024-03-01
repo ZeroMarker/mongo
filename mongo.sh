f94c8cc304ffc96dd2d845df100fbc8a94cf00f4
ZeroMarker <u78k4l67@163.com>
2024-03-01 17:00:41 +0800

    Auto commit

--- mongo.sh
+++ mongo.sh
@@ -5,3 +5,69 @@ use moviedb;
 db.dropDatabase();
 
 show collections;
+
+# like find
+db.meta.find({Name:/大/})
+
+db.meta.find({Profession:"TS"})
+
+db.meta.find({Age:{$gte:12,$lte:45})
+
+db.movies.find({releaseDate:{$gte:"2022-10-01",$lte:"2023-09-01"})
+
+db.inventory.find( { item: { $not: /^p.*/ } } )
+
+db.inventory.find( {
+    $and: [
+        { $or: [ { qty: { $lt : 10 } }, { qty : { $gt: 50 } } ] },
+        { $or: [ { sale: true }, { price : { $lt : 5 } } ] }
+    ]
+} )
+
+db.movies.aggregate([
+  {
+    $match: { title: /the/ }
+  },
+  {
+    $lookup: {
+      from: 'reviews',
+      localField: 'reviewIds',
+      foreignField: '_id',
+      as: 'reviews'
+    }
+  },
+  {
+    $unwind: '$reviews'
+  },
+  {
+    $group: {
+      _id: '$_id',
+      title: { $first: '$title' },
+      reviews: { $push: '$reviews' }
+    }
+  }
+])
+
+db.movies.aggregate([
+  {
+    $lookup: {
+      from: 'reviews',
+      localField: 'reviewIds',
+      foreignField: '_id',
+      as: 'reviews'
+    }
+  },
+  {
+    $unwind: '$reviews'
+  },
+  {
+    $project: {
+      _id: 0,
+      'movieId': '$_id',
+      'movieTitle': '$title',
+      'reviewId': '$reviews._id',
+      'reviewText': '$reviews.body'
+    }
+  }
+]).pretty();
+
--- mongo.sql
+++ mongo.sql
@@ -1,3 +1,27 @@
 select *
 from moviedb.movies;
 
+
+db.movies.aggregate([
+  {
+    $match: { title: /the/ }
+  },
+  {
+    $lookup: {
+      from: 'reviews',
+      localField: 'reviewIds',
+      foreignField: '_id',
+      as: 'reviews'
+    }
+  },
+  {
+    $unwind: '$reviews'
+  },
+  {
+    $group: {
+      _id: '$_id',
+      title: { $first: '$title' },
+      reviews: { $push: '$reviews' }
+    }
+  }
+])
